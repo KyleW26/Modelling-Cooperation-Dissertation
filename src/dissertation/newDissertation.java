43d3333764febc6cb51b220faa114d6926488ea6
Kyle <eeu8e1@bangor.ac.uk>
2018-10-31 15:08:28 +0000

    Initial Commit
    First Commit

---
+++ src/dissertation/newDissertation.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dissertation;
+
+/**
+ *
+ * @author kylewilliams
+ */
+public class newDissertation {
+
+    // Generate 3 different strategies
+    static char[] strategyArray = {'C', 'D', 'C', 'C', 'C', 'D', 'D'};
+
+    // Generate 3 different groups
+    static boolean[][] groupArray = {
+        {true, true, false, true, false, false, true},
+        {true, false, true, true, false, false, false},
+        {true, true, true, true, true, false, true}};
+    
+    public void calculatePayoff() {
+        hhhh
+    }
+    
+    public void chooseRandom() {
+        
+    }
+            
+            
+    public static void main(String[] args) {
+        
+    }
+
+}
