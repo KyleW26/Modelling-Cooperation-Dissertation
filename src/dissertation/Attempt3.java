0c367add4786d594a547580007ad60078d2ba38d
Kyle <eeu8e1@bangor.ac.uk>
2018-11-20 12:25:01 +0000

    Created new FOR loops to iterate through a 2D array
    Title descibes it all

--- src/dissertation/Attempt3.java
+++ src/dissertation/Attempt3.java
@@ -21,6 +21,8 @@ public class Attempt3 {
         {true, false, true, true, false, false, false},
         {true, true, true, true, true, false, true}};
 
+    static boolean memberOf[] = new boolean[3];
+
     static int randomPerson = 0; // Individual and Group
     static double r = 2.0; // Multiplication Value
     static double Nc = 0.0; // Number of cooperators
@@ -41,48 +43,59 @@ public class Attempt3 {
         randomPerson = rand.nextInt(7) + 0;
     }
 
-    // int i = individual ... int k = group
+    // int i = individual ... int g = group
     public static void calculateGroup() {
 
-        /*for (int row = 0; row < groupArray.length; row++) {
-            sum = 0;
-            for (int column = 0; column < groupArray[row].length; column++) {
-                if(groupArray[row][randomPerson] == true) {
-                    N++;
-                }
-
-                //System.out.print("\t" + groupArray[row][column] + "\t|");
-            }
-            // System.out.println();
+        // Groups (rows)
+        /*for (int g = 0; g < groupArray.length; g++) {
+        // Individuals (columns)
+        for (int i = 0; i < groupArray[1].length; i++) {
+        if (groupArray[g][i] == true) {
+        N++;
+        }
+        //System.out.println(groupArray[g][i]);
+        }
         } */
- /*  for G [row][column]
-                sum = 0;
-                    for K = 0:2
-                        IF col belongs to row
-                                sum += payoff(k)
-                        end
-                    end
-        end */
- 
-        for (int g = 0; g < groupArray.length; g++) {
+        
+        memberOf[0] = groupArray[0][randomPerson] == true;
+        memberOf[1] = groupArray[1][randomPerson] == true;
+        memberOf[2] = groupArray[2][randomPerson] == true;
+
+        System.out.println("Individual " + randomPerson + " is a member of groups: "
+                + memberOf[0] + " " + memberOf[1] + " " + memberOf[2]);
+
+        // Iterate through memberOf array
+        for (int i = 0; i < memberOf.length; i++) {
             sum = 0;
-            for (int i = 0; i < groupArray[1].length; i++) {
-                if (groupArray[g][randomPerson] == true) {
-                    //sum += payoff(k);
-                    N++;
+            if (memberOf[i] == true) {
+                // Iterate through groupArray
+                for (int j = 0; j < groupArray[1].length; j++) {
+                    if (groupArray[i][j] == true) {
+                        N++;
+                    }
                 }
             }
         }
+
+        /*int counter = 0;
+        // Groups
+        for (int i = 0; i < groupArray.length; i++) {
+            // Individuals
+            for (int j = 0; j < groupArray[1].length; j++) {
+                if (groupArray[i][j]) {
+                    counter++;
+                }
+            }
+        }
+
+        // Print the total amount of trues and falses
+        System.out.println("true: " + counter);
+        System.out.println("false: " + (groupArray[1].length * groupArray.length - counter)); */
         
-
-        System.out.println(groupArray.length);
-        System.out.println(groupArray[1].length);
-
-        System.out.println(N);
-
         System.out.println(" ");
         System.out.println("Random Individual: " + randomPerson);
         System.out.println("Value of N: " + N);
+        System.out.println("Value of Nc: " + Nc);
         System.out.println("Amount of groups: " + groupArray.length); // Groups
     }
 }
