14e95eda817917322450977746cdc22fb4869e4c
Kyle <eeu8e1@bangor.ac.uk>
2018-11-21 10:41:26 +0000

    Created working FOR loops
    Calculates N and Nc correctly.

--- src/dissertation/Attempt3.java
+++ src/dissertation/Attempt3.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dissertation;
 
 import java.util.Random;
@@ -25,8 +20,8 @@ public class Attempt3 {
 
     static int randomPerson = 0; // Individual and Group
     static double r = 2.0; // Multiplication Value
-    static double Nc = 0.0; // Number of cooperators
-    static double N = 0.0; // Number of individuals
+    //static double Nc = 0.0; // Number of cooperators
+    //static double N = 0.0; // Number of individuals
     static double c = 10.0; // Cost of contributions
     static double PD, PC, sum;
 
@@ -45,18 +40,28 @@ public class Attempt3 {
 
     // int i = individual ... int g = group
     public static void calculateGroup() {
+        int individual = groupArray[0].length;
 
         // Groups (rows)
-        /*for (int g = 0; g < groupArray.length; g++) {
-        // Individuals (columns)
-        for (int i = 0; i < groupArray[1].length; i++) {
-        if (groupArray[g][i] == true) {
-        N++;
+        for (int g = 0; g < groupArray.length; g++) {
+            double Nc = 0;
+            double N = 0;
+            double sum = 0;
+            // Individuals (columns)
+            for (int i = 0; i < individual; i++) {
+                if (groupArray[g][i] == true) {
+                    N++;
+                    if(strategyArray[i] == 'C') {
+                        Nc++;
+                    }
+                }
+                //System.out.println(groupArray[g][i]);
+            }
+            System.out.println("N: "+N);
+            System.out.println("Nc: "+Nc);
+            //PD = (r * Nc * c)/N;
+            //PC = PD - c;
         }
-        //System.out.println(groupArray[g][i]);
-        }
-        } */
-        
         memberOf[0] = groupArray[0][randomPerson] == true;
         memberOf[1] = groupArray[1][randomPerson] == true;
         memberOf[2] = groupArray[2][randomPerson] == true;
@@ -64,7 +69,7 @@ public class Attempt3 {
         System.out.println("Individual " + randomPerson + " is a member of groups: "
                 + memberOf[0] + " " + memberOf[1] + " " + memberOf[2]);
 
-        // Iterate through memberOf array
+        /*// Iterate through memberOf array
          for (int i = 0; i < memberOf.length; i++) {
             sum = 0;
             if (memberOf[i] == true) {
@@ -75,11 +80,10 @@ public class Attempt3 {
                     }
                 }
             }
-        }
-         
+        } */
         System.out.println("Random Individual: " + randomPerson);
-        System.out.println("Value of N: " + N);
-        System.out.println("Value of Nc: " + Nc);
+        //System.out.println("Value of N: " + N);
+        //System.out.println("Value of Nc: " + Nc);
         System.out.println("Amount of groups: " + groupArray.length); // Groups
     }
 }
