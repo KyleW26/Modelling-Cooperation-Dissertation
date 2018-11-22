05ec725629d26ef6c796d0dd3c9c3a5d9713eae5
Kyle <eeu8e1@bangor.ac.uk>
2018-11-22 12:38:48 +0000

    Calculating neighbour payoff

--- src/dissertation/Attempt3.java
+++ src/dissertation/Attempt3.java
@@ -31,6 +31,7 @@ public class Attempt3 {
     public static void main(String[] args) {
         getRandom();
         calculateGroup();
+        checkNeighbours();
     }
 
     public static void getRandom() {
@@ -69,8 +70,6 @@ public class Attempt3 {
                 }
             }
         }
-        System.out.println("Sum: " + sum);
-        System.out.println();
 
         memberOf[0] = groupArray[0][randomPerson] == true;
         memberOf[1] = groupArray[1][randomPerson] == true;
@@ -79,5 +78,90 @@ public class Attempt3 {
         System.out.println("Random Individual: " + randomPerson);
         System.out.println("Individual " + randomPerson + " is a member of groups: "
                 + memberOf[0] + " " + memberOf[1] + " " + memberOf[2]);
+        System.out.println("Sum: " + sum);
     }
+
+    public static void checkNeighbours() {
+        // Get the neighbour
+        int leftNeighbour = randomPerson - 1;
+        int rightNeighbour = randomPerson + 1;
+
+        // Error checking
+        if (rightNeighbour >= 7) {
+            System.out.println("There is no neighbour on the right, checking individual 6.");
+            rightNeighbour = 0;
+        }
+        if (leftNeighbour < 0) {
+            System.out.println("There is no neighbour on the left, checking individual 0.");
+            leftNeighbour = 0;
+        }
+
+        // Get the strategy
+        char leftNeighbourStrat = strategyArray[leftNeighbour];
+        char rightNeighbourStrat = strategyArray[rightNeighbour];
+
+        System.out.println();
+        System.out.println("Individual " + leftNeighbour + " = " + leftNeighbourStrat);
+        System.out.println("Individual " + rightNeighbour + " = " + rightNeighbourStrat);
+
+        // Length of array & calculations for payoff
+        int individual = groupArray[0].length;
+        double sumLeft = 0.0;
+        double sumRight = 0.0;
+        // Groups (rows)
+        for (int g = 0; g < groupArray.length; g++) {
+            double Nc = 0;
+            double N = 0;
+            // Individuals (columns)
+            for (int i = 0; i < individual; i++) {
+                if (groupArray[g][i] == true) {
+                    N++;
+                    if (strategyArray[i] == 'C') {
+                        Nc++;
+                    }
+                }
+            }
+
+            if (groupArray[g][leftNeighbour] == true) {
+                PD = (r * Nc * c) / N;
+                PC = PD - c;
+
+                if (strategyArray[leftNeighbour] == 'C') {
+                    sumLeft += PC;
+                } else {
+                    sumLeft += PD;
+                }
+            }
+
+            if (groupArray[g][rightNeighbour] == true) {
+                PD = (r * Nc * c) / N;
+                PC = PD - c;
+
+                if (strategyArray[rightNeighbour] == 'C') {
+                    sumRight += PC;
+                } else {
+                    sumRight += PD;
+                }
+            }
+
+        }
+        // Print out the sums of the left & right neighbour
+        System.out.println("Sum 2: " + sumLeft);
+        System.out.println("Sum 3: " + sumRight);
+        System.out.println();
+
+        // Change the strategy array based upon values being higher or lower
+        if (sumLeft > sum) {
+            strategyArray[randomPerson] = leftNeighbourStrat;
+        }
+        if (sumRight > sum) {
+            strategyArray[randomPerson] = rightNeighbourStrat;
+        }
+
+        // Print out the strategy array
+        for (int x = 0; x < strategyArray.length; x++) {
+            System.out.print(strategyArray[x] + " ");
+        }
+    }
+
 }
